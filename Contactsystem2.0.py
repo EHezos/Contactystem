97956acccfb7c140134a57ea323e5259e2adaf96
Eavheang Oeng <87308487+EHezos@users.noreply.github.com>
2023-07-12 14:39:36 +0700

    Contactsystem2.0.py
    Fixing table not create

--- Contactsystem2.0.py
+++ Contactsystem2.0.py
@@ -1,22 +1,16 @@
-#Import the mysql dictionary
-import mysql.connector
+import psycopg2
 import PySimpleGUI as sg
-
-sg.theme('DarkGreen3')
-
 #connect to local server
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="heang",
-  password="2005",
-  database = "contact"
-)
+mydb = psycopg2.connect(database="postgres", user = "postgres", password = "2005", host = "localhost", port = "5432")
 mycursor = mydb.cursor()
+def create_table():
+    mycursor.execute('''CREATE TABLE IF NOT EXISTS contact(NAME TEXT NOT NULL,JOB TEXT NOT NULL,SEX TEXT NOT NULL,Phonenumber INT NOT NULL)''')
 
+create_table()
 #Function to insert contact into the dbms
-def insert(name, job, sex, phonenumber):
+def insert(name, job, sex, phone):
  insert = "INSERT INTO contact (name, job, sex, phonenumber) VALUES(%s,%s,%s,%s)"
- data = (name, job, sex, phonenumber)
+ data = (name, job, sex, phone)
  mycursor.execute(insert,data)
  print("Contact is added")
  mydb.commit()
@@ -49,7 +43,7 @@ layout = [
     [sg.Text("Job:"), sg.Input(key="-JOB-")],
     [sg.Text("Sex:"), sg.Input(key="-SEX-")],
     [sg.Text("Phone Number:"), sg.Input(key="-PHONE-")],
-    [sg.Button("Insert Contact"), sg.Button("Delete by Name Contact"), sg.Button("Show Contact"), sg.Button("Search Contact by Name")],
+    [sg.Button("Insert Contact"), sg.Button("Delete Contact"), sg.Button("Show Contact"), sg.Button("Search Contact by Name")],
     [sg.Output(size=(50,10))]
 ]
 
